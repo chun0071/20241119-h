c89cc5030a9d1af926bca463b40c4337f94a20ac
User <User@DESKTOP-JO6NS6S>
2024-11-23 18:34:00 +0800

    good

--- 20241119-h/20241119-h.cpp
+++ 20241119-h/20241119-h.cpp
@@ -1,7 +1,20 @@
-﻿#include <iostream>
+﻿#include <string>
+#include <iostream>
+#include "Person.h"
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+	Person person1("A123456789", "Chen", "Jason", "M", "1999-01-01");
 
+	Person* person2 = new Person();
+	person2->setId("B987654321");
+	person2->setLastName("Wang");
+	person2->setFirstName("David");
+	person2->setGender("M");
+	person2->setBirthDate("1999-12-31");
+
+	person1.display();
+	cout << endl;
+	person2->display();
+}
